97003b1fc11de0571aa2cd6c59d1c0f43785d279
pamela.glownia <pamela.glownia@gmail.com>
2022-02-01 21:50:00 +0100

    Initial commit

---
+++ src/main/java/pl/glownia/pamela/FriendlyPetClinic/FriendlyPetClinicApplication.java
@@ -0,0 +1,13 @@
+package pl.glownia.pamela.FriendlyPetClinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FriendlyPetClinicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FriendlyPetClinicApplication.class, args);
+	}
+
+}
