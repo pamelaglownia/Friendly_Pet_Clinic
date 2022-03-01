fe977afc7db067bea979b6e5ad053423fb544a18
pamela.glownia <pamela.glownia@gmail.com>
2022-03-01 19:50:33 +0100

    Get vets

--- src/main/java/pl/glownia/pamela/FriendlyPetClinic/vet/VetController.java
+++ src/main/java/pl/glownia/pamela/FriendlyPetClinic/vet/VetController.java
@@ -4,6 +4,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+import java.util.Optional;
+
 @RestController
 @RequestMapping("/vets")
 public class VetController {
@@ -19,5 +22,15 @@ public class VetController {
     @ResponseStatus(HttpStatus.OK)
     void createVet(@RequestBody VetDto vetDto) {
         vetService.createVet(vetDto);
+    }
+
+    @GetMapping
+    List<VetDto> getAllVets() {
+        return vetService.getAllVets();
+    }
+
+    @GetMapping("/{vetId}")
+    Optional<VetDto> getVetById(@PathVariable long vetId) {
+        return vetService.getVetById(vetId);
     }
 }
--- src/main/java/pl/glownia/pamela/FriendlyPetClinic/vet/VetService.java
+++ src/main/java/pl/glownia/pamela/FriendlyPetClinic/vet/VetService.java
@@ -3,6 +3,10 @@ package pl.glownia.pamela.FriendlyPetClinic.vet;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
 @Service
 public class VetService {
     private final VetRepository vetRepository;
@@ -15,6 +19,16 @@ public class VetService {
     public void createVet(VetDto vetDto) {
         VetEntity vetEntity = convertToEntity(vetDto);
         vetRepository.save(vetEntity);
+    }
+
+    List<VetDto> getAllVets() {
+        List<VetEntity> vets = vetRepository.findAll();
+        return vets.stream().map(this::convertToDto).collect(Collectors.toList());
+    }
+
+    Optional<VetDto> getVetById(long id) {
+        VetEntity vetEntity = vetRepository.findById(id).orElseThrow(() -> new RuntimeException("Vet with id " + id + " doesn't exist."));
+        return Optional.of(convertToDto(vetEntity));
     }
 
     //convert to DTO
